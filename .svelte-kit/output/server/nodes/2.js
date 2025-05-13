777ed277d1657a3eaed88698fdfd96fb2b3ace55
George <george@Georges-MacBook-Air.local>
2025-05-13 11:52:49 +0300

    Remove .DS_Store and .svelte-kit/ from repo

--- .svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.neZ0Bab7.js","_app/immutable/chunks/e8IMUzB9.js","_app/immutable/chunks/BWCsPxKq.js","_app/immutable/chunks/C8M7huGC.js","_app/immutable/chunks/Com66ilP.js","_app/immutable/chunks/CeWid8xA.js","_app/immutable/chunks/D8ZUucn5.js"];
-export const stylesheets = ["_app/immutable/assets/2.D31q9EK5.css"];
-export const fonts = [];
